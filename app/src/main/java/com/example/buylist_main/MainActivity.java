5cd972b083939e9d44d0ec12ca8a4f506acce7c3
Dophamino <dophamino@gmail.com>
2021-06-22 10:16:36 +0400

    New edit

--- app/src/main/java/com/example/buylist_main/MainActivity.java
+++ app/src/main/java/com/example/buylist_main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.buylist_main;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void main_addClick(View view) {
+        Intent add_intent = new Intent(this, activity_addList.class);
+        startActivity(add_intent);
     }
 }
